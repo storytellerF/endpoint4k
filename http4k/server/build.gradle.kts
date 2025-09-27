205dd84a1122be5337d85afccdd51e8b3c4db9b4
storytellerF <34095089+storytellerF@users.noreply.github.com>
2025-09-27 21:01:32 +0800

    update version

--- http4k/server/build.gradle.kts
+++ http4k/server/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.1.21"
+    id("common-publish")
 }
 
 dependencies {
--- jitpack-publish.sh
+++ jitpack-publish.sh
@@ -1,1 +1,2 @@
-./gradlew publishToMavenLocal -Pversion=1.0.0 -Pgroup=com.github.storytellerF.route4k
+#!/usr/bin/env sh
+./gradlew publishToMavenLocal -Pversion=1.1.0 -Pgroup=com.github.storytellerF.route4k
