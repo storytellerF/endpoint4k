71d1199c82bfc1c333ac1c4c42cfaa8965f1e9ea
storytellerF <34095089+storytellerF@users.noreply.github.com>
2026-02-01 20:52:19 +0800

    add comment

--- common-publish/src/main/kotlin/common-publish.gradle.kts
+++ common-publish/src/main/kotlin/common-publish.gradle.kts
@@ -5,12 +5,16 @@ plugins {
 group = property("group") ?: "com.storyteller_f.endpoint4k"
 version = property("version") ?: "1.0-SNAPSHOT"
 
+println("group: $group, version: $version")
+
 publishing {
     publications {
+        // 如果不添加下面的代码，无法发布出来artifact
         create<MavenPublication>("mavenJava") {
             artifactId = project.displayName.substringAfter("project ").removeSurrounding("'").removePrefix(":")
                 .replace(":", "-")
-            from(components["java"])
+            // 包含java kotlin，选择kotlin 也能发布成功
+            from(components["kotlin"])
         }
     }
 }
